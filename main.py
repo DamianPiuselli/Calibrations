55586d4be64e8566a7bfdabbb9e05b52bd4b0ebc
Unknown <sir.damm@gmail.com>
2018-03-18 23:56:29 -0300

    21

--- main.py
+++ main.py
@@ -3,8 +3,9 @@
 Multiple linear calibration
 y = m1*x1 + m1*x1 + ... + b
 y = señal
-xi = concentraciones
-mx = f. de respuesta
+xi = concentration i
+mi = sensibility i
+b = constant
 """
 
 import statsmodels.api as sm
@@ -14,38 +15,26 @@ import matplotlib.pyplot as plt
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
 
-# Cargando los datos desde excel usando pandas.
+# Loading dummy data.
+df = pd.read_excel("dummy data.xls")
+y = df["señal"]
+x = df[["concentracion 1","concentracion 2"]]
 
-df = pd.read_excel("dataset calibracion linear multiple.xlsx")
-y_data = df["señal"]
-x_data = df[["concentracion 1", "concentracion 2", "concentracion 3"]]
+# Defining class Linear Regression (OLS)
 
-# Modelo
-
-x_data = sm.add_constant(x_data)   # agrega la ordenada al origen (b)
-
-model = sm.OLS(y_data, x_data).fit()
-predictions = model.predict(x_data)
-print(model.summary())
-
-# Graficos
-
-#prstd, iv_l, iv_u = wls_prediction_std(model)
-#
-#fig, ax = plt.subplots(figsize=(8,6))
-#
-#ax.plot(x_data, y_data, 'o', label="data")
-#ax.plot(x_data, model.fittedvalues, 'r--.', label="OLS")
-#ax.plot(x_data, iv_u, 'r--')
-#ax.plot(x_data, iv_l, 'r--')
-#ax.legend(loc='best');
-
-prstd, iv_l, iv_u = wls_prediction_std(model)
-
-fig, ax = plt.subplots(figsize=(8,6))
-
-ax.plot(x_data["concentracion 1"], y_data, 'o', label="data")
-ax.plot(x_data["concentracion 1"], model.fittedvalues, 'o', label="OLS")
-#ax.plot(x_data["concentracion 1"], iv_u, 'r--')
-#ax.plot(x_data["concentracion 1"], iv_l, 'r--')
-ax.legend(loc='best');
+class linear:
+    """Multiple Linear Regression clas"""    
+    def __init__(self, y_data, x_data, constant=True):
+        self.Y = y_data
+        self.X = x_data
+        self.constant = constant
+        if self.constant == True:
+            self.X = sm.add_constant(self.X)
+        
+        self.model = sm.OLS(self.Y, self.X)
+        self.results = self.model.fit()
+        self.summary = self.results.summary()
+        
+    def ccurve(self):
+        
+        plt.
